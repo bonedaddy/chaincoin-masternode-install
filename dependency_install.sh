3b47b813e2b4146054be1a306c78e80d7d2da6f1
Marcelo Lipienski <marcelo.lipienski@gmail.com>
2017-07-15 18:38:42 -0300

    Update dependency_install.sh

--- dependency_install.sh
+++ dependency_install.sh
@@ -1,20 +1,9 @@
 #! /bin/bash
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install automake -y
-sudo apt-get install libdb++-dev -y
-sudo apt-get install build-essential libtool autotools-dev -y
-sudo apt-get install autoconf pkg-config libssl-dev -y
-sudo apt-get install libboost-all-dev -y
-sudo apt-get install libminiupnpc-dev -y
-sudo apt-get install git -y
-sudo apt-get install software-properties-common -y
-sudo apt-get install python-software-properties -y
-sudo apt-get install g++ -y
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update -y
-sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+sudo apt-get upgrade -y
+sudo apt-get install automake autoconf autotools-dev build-essential git g++ libboost-all-dev libdb4.8-dev libdb4.8++-dev libdb++-dev libminiupnpc-dev libtool libssl-dev pkg-config python-software-properties software-properties-common -y
 cd ~
 mkdir chaincoin-source
 cd chaincoin-source
