9d77e6fddd2dd66aa4d6426c758ec073a127beca
post-fix <alextrottier2014@gmail.com>
2017-07-19 19:24:09 -0700

    Adding some updates to make chaincoind.sh more automated  Please enter the commit message for your changes. Lines starting

--- chaincoind.sh
+++ chaincoind.sh
@@ -13,7 +13,7 @@ echo "This may mean you might have to send 1000.001CHC"
 echo "Sleeping for 60 seconds"
 sleep 60
 echo "Generate masternode key and saving it to a file"
-chaincoind masternode genkey > chaincoind_masternode_key
+chaincoind masternode genkey > "~/chaincoind_masternode_key"
 echo "Stopping chaincoind"
 chaincoind stop 
 echo "Sleeping for 60 seconds"
@@ -22,7 +22,7 @@ echo "listen=1" >> ~/.chaincoin/chaincoin.conf
 echo "masternode=1" >> ~/.chaincoin/chaincoin.conf
 echo "masternodeprivkey=$(cat chaincoind_masternode_key)" >> ~/.chaincoin/chaincoin.conf
 echo "masternodeaddr=$primary_interface_ip:11994" >> ~/.chaincoin/chaincoin.conf
-chaincoind --daemon &
+chaincoind --daemon 
 echo "Waiting for 15 transactions. Sleeping for 1800 seconds"
 sleep 1800
 chaincoind masternode start
